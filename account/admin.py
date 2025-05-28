de8c13c2cc834119b8fca529cba08c476495a121
SABARI S B <earc.ethereum@gmail.com>
2025-05-28 17:38:03 +0530

    admin_panal

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Category
- 
 
-
-admin.site.register(Category)
 # Register your models here.
--- category/models.py
+++ category/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    category_name=models.CharField(max_length=100, unique=True)
+    slug=models.CharField(max_length=100,unique=True)
+    description=models.TextField(max_length=250)
+    cat_img=models.ImageField(upload_to='photos/categories', blank=True, null=True)
+
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        ordering = ['category_name'] 
+    def __str__(self):
+        return self.category_name
+    
