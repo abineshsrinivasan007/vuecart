233815ee06411623a717274514498113ba6e1a6f
SABARI S B <earc.ethereum@gmail.com>
2025-05-29 17:58:24 +0530

    admin-panel

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
-admin.site.register(Account)
+
+
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'date_joined', 'last_login', 'is_active', 'is_staff')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    readonly_fields = ('date_joined', 'last_login')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
 # Register your models here.
