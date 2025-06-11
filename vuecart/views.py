475949af3884bdee8bad3f80c16c236e0522fdab
SABARI S B <earc.ethereum@gmail.com>
2025-06-11 16:55:10 +0530

    file-update

--- vuecart/views.py
+++ vuecart/views.py
@@ -6,9 +6,7 @@ from store.models import Product
 # Create your homepage views here.
 def home(request):
     products=Product.objects.all().filter(is_available=True)
-    
     context={
         'products': products
     }
-    print(context,"abinesh")
     return render(request, 'home.html', context)
